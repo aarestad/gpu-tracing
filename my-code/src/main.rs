03c85bb4550a4fa9f2883d90d50ad78577ef5943
Peter Aarestad <peter@landscapehub.com>
2025-11-27 16:54:35 -0700

    stop trying to be cute with newer version of winit

--- my-code/src/main.rs
+++ my-code/src/main.rs
@@ -1,53 +1,100 @@
 use {
-    anyhow::Result,
-    winit::application::ApplicationHandler,
-    winit::event::WindowEvent,
-    winit::event_loop::{ActiveEventLoop, ControlFlow, EventLoop},
-    winit::window::{Window, WindowId},
+    anyhow::{Context, Result},
+    winit::{
+        event::{Event, WindowEvent},
+        event_loop::{ControlFlow, EventLoop},
+        window::{Window, WindowBuilder},
+    },
 };
 
-const SIZE: winit::dpi::PhysicalSize<i32> = winit::dpi::PhysicalSize::new(800, 600);
+const WIDTH: u32 = 800;
+const HEIGHT: u32 = 600;
 
-#[derive(Default)]
-struct App {
-    window: Option<Window>,
+async fn connect_to_gpu(window: &Window) -> Result<(wgpu::Device, wgpu::Queue, wgpu::Surface<'_>)> {
+    use wgpu::TextureFormat::{Bgra8Unorm, Rgba8Unorm};
+
+    // Create an "instance" of wgpu. This is the entry-point to the API.
+    let instance = wgpu::Instance::default();
+
+    // Create a drawable "surface" that is associated with the window.
+    let surface = instance.create_surface(window)?;
+
+    // Request a GPU that is compatible with the surface. If the system has multiple GPUs then
+    // pick the high performance one.
+    let adapter = instance
+        .request_adapter(&wgpu::RequestAdapterOptions {
+            power_preference: wgpu::PowerPreference::HighPerformance,
+            force_fallback_adapter: false,
+            compatible_surface: Some(&surface),
+        })
+        .await
+        .context("failed to find a compatible adapter")?;
+
+    // Connect to the GPU. "device" represents the connection to the GPU and allows us to create
+    // resources like buffers, textures, and pipelines. "queue" represents the command queue that
+    // we use to submit commands to the GPU.
+    let (device, queue) = adapter
+        .request_device(&wgpu::DeviceDescriptor::default())
+        .await
+        .context("failed to connect to the GPU")?;
+
+    // Configure the texture memory backing the surface. Our renderer will draw to a surface
+    // texture every frame.
+    let caps = surface.get_capabilities(&adapter);
+    let format = caps
+        .formats
+        .into_iter()
+        .find(|it| matches!(it, Rgba8Unorm | Bgra8Unorm))
+        .context("could not find preferred texture format (Rgba8Unorm or Bgra8Unorm)")?;
+    let size = window.inner_size();
+    let config = wgpu::SurfaceConfiguration {
+        usage: wgpu::TextureUsages::RENDER_ATTACHMENT,
+        format,
+        width: size.width,
+        height: size.height,
+        present_mode: wgpu::PresentMode::AutoVsync,
+        alpha_mode: caps.alpha_modes[0],
+        view_formats: vec![],
+        desired_maximum_frame_latency: 3,
+    };
+    surface.configure(&device, &config);
+
+    Ok((device, queue, surface))
 }
 
-impl ApplicationHandler for App {
-    fn resumed(&mut self, event_loop: &ActiveEventLoop) {
-        self.window = Some(
-            event_loop
-                .create_window(
-                    Window::default_attributes()
-                        .with_inner_size(SIZE)
-                        .with_resizable(false)
-                        .with_title("GPU Path Tracer".to_string()),
-                )
-                .unwrap(),
-        );
-    }
+#[pollster::main]
+async fn main() -> Result<()> {
+    let event_loop = EventLoop::new()?;
+    let window_size = winit::dpi::PhysicalSize::new(WIDTH, HEIGHT);
+    let window = WindowBuilder::new()
+        .with_inner_size(window_size)
+        .with_resizable(false)
+        .with_title("GPU Path Tracer".to_string())
+        .build(&event_loop)?;
+    let (_device, _queue, surface) = connect_to_gpu(&window).await?;
 
-    fn window_event(&mut self, event_loop: &ActiveEventLoop, _id: WindowId, event: WindowEvent) {
+    // TODO: initialize renderer
+
+    event_loop.run(|event, control_handle| {
+        control_handle.set_control_flow(ControlFlow::Poll);
         match event {
-            WindowEvent::CloseRequested => {
-                println!("The close button was pressed; stopping");
-                event_loop.exit();
-            }
-            WindowEvent::RedrawRequested => {
-                // TODO: draw frame
-                self.window.as_ref().unwrap().request_redraw();
-            }
+            Event::WindowEvent { event, .. } => match event {
+                WindowEvent::CloseRequested => control_handle.exit(),
+                WindowEvent::RedrawRequested => {
+                    // Wait for the next available frame buffer.
+                    let frame: wgpu::SurfaceTexture = surface
+                        .get_current_texture()
+                        .expect("failed to get current texture");
+
+                    // TODO: draw frame
+
+                    frame.present();
+                    window.request_redraw();
+                }
+                _ => (),
+            },
             _ => (),
         }
-    }
-}
-
-fn main() -> Result<()> {
-    let event_loop = EventLoop::new()?;
-    event_loop.set_control_flow(ControlFlow::Poll);
-
-    let mut app = App::default();
-    event_loop.run_app(&mut app)?;
-
+    })?;
     Ok(())
 }
