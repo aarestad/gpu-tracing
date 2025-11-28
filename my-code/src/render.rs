1fbf3d4c77827596da53c44bd53e0f4ee04497ec
Peter Aarestad <peter@landscapehub.com>
2025-11-27 17:52:18 -0700

    less-simple shader

--- my-code/src/render.rs
+++ my-code/src/render.rs
@@ -48,7 +48,7 @@ impl PathTracer {
         render_pass.set_pipeline(&self.display_pipeline);
 
         // Draw 1 instance of a polygon with 3 vertices.
-        render_pass.draw(0..3, 0..1);
+        render_pass.draw(0..6, 0..1);
 
         // End the render pass by consuming the object.
         drop(render_pass);
